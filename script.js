dd50b7b6ae88dbba08cfdc9625c3ae6a5d42e899
Vedangi-11 <128711011+Vedangi-11@users.noreply.github.com>
2024-02-29 23:05:59 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-    const verticalScrollPx = window.scrollY || window.pageYOffset;
-  
-    if (verticalScrollPx > 500) {
-      document.getElementsByClassName(headerr).style.backgroundColor = '#f00f64e3';
-    } else {
-      document.getElementsByClassName(headerr).style.backgroundColor = 'transparent';
-    }
-  });
